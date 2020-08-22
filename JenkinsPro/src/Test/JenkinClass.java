7c47d26c5deaef1ddc9fb9fc646f9d9e16a4860e
pc <pc@hp>
2020-08-22 13:31:14 +0530

    coding change

--- JenkinsPro/src/Test/JenkinClass.java
+++ JenkinsPro/src/Test/JenkinClass.java
@@ -1,48 +1,49 @@
 package Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class JenkinClass {
   @Test
-  public void f() throws InterruptedException {
-	  
+  public void wbtbl() {
 	  System.setProperty("webdriver.chrome.driver", "E:\\Markose\\chromedriver.exe");
-
 	  WebDriver driver = new ChromeDriver();
-
+	  
 	  driver.manage().deleteAllCookies();
 	  driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
-
-	  driver.get("https://www.facebook.com/");
-
+	  
+	  driver.navigate().to("https://erail.in/trains-between-stations/mumbai-central-BCT/new-delhi-NDLS");
+	  
 	  driver.manage().window().maximize();
-
-	  Select slt = new Select(driver.findElement(By.xpath("//select[@title='Day']")));
-	  slt.selectByIndex(19);
-
-	  Thread.sleep(3000);
-	  	  
-	  Select slt1 = new Select(driver.findElement(By.xpath("//select[@id='month']")));
-	  slt1.selectByValue("4");
-
-	  Thread.sleep(3000);
-
-	  Select slt2 = new Select(driver.findElement(By.xpath("//select[@name='birthday_year']")));
-	  slt2.selectByVisibleText("1985");
-
-	  Thread.sleep(3000);
-	  slt2.deselectAll();
-
-
-
-	    }
+	  
+	  WebElement tab = driver.findElement(By.xpath("//table[@class='DataTable DataTableHeader TrainList TrainListHeader']"));
+	  
+	  List<WebElement> rows = tab.findElements(By.tagName("tr"));
+	  
+	  System.out.println("The list of rows:" +rows.size());
+	  
+	  for(WebElement row : rows) {
+		  
+		  List<WebElement> cells = tab.findElements(By.tagName("td"));
+		  
+		  for(WebElement cell:cells) {
+			  
+			  String value = cell.getText();
+			  
+			  System.out.print(value);
+		  }
+	  System.out.println();
+	  }
+	  
+	  driver.close();
+		  }	
   }
 
